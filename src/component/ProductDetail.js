8c0d399735d15fe82d678f97eea59caa451847d4
saranam <hjk786@naver.com>
2022-09-14 23:00:24 +0900

    상세페이지 컴포넌트 작업 중

---
+++ src/component/ProductDetail.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function ProductDetail() {
+    return <ProductDetail1></ProductDetail1>;
+}
+
+const ProductDetail1 = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import ProductDetail from "../component/ProductDetail";
 
 export default function Detail() {
-  return (
-    <div className='Detail'>
-        상세페이지
-    </div>
-  )
+    return (
+        <DetailPage>
+            <ProductDetail />
+        </DetailPage>
+    );
 }
+
+const DetailPage = styled.div`
+    position: relative;
+    width: 100%;
+`;
