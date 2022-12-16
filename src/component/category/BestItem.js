9860d4622cf73bdc8b0498853961f26724751fb6
saranam <hjk786@naver.com>
2022-12-16 12:38:17 +0900

    Updates

--- src/component/category/BestItem.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-
-export default function BestItem() {
-    return <BestItemContainer>베스트</BestItemContainer>;
-}
-const BestItemContainer = styled.div``;
